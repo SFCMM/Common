56e193a8b4ae90ab8ec394ba6e0d99d14c3a6875
svchb <berger.sven@gmail.com>
2021-08-11 21:34:45 +0200

    add log.h

--- include/sfcmm_common.h
+++ include/sfcmm_common.h
@@ -9,6 +9,7 @@ static constexpr int MAX_DIM = 4;
 #include "common/compiler_config.h"
 #include "common/globalmpi.h"
 #include "common/macros.h"
+#include "common/log.h"
 #include "common/sfcmm_types.h"
 
 #include "common/util/backtrace.h"
