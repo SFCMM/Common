df651102cfed0652e8ab053fce20dd444dae67a4
svchb <berger.sven@gmail.com>
2021-08-11 21:21:13 +0200

    add macros.h

---
+++ include/common/macros.h
@@ -0,0 +1,45 @@
+#ifndef GRIDGENERATOR_MACROS_H
+#define GRIDGENERATOR_MACROS_H
+
+#ifdef CLANG_COMPILER
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-pragmas"
+#pragma ide diagnostic   ignored "cppcoreguidelines-macro-usage"
+#endif
+
+
+/// Define macros to stringify literal and expanded macro arguments
+///
+/// STRINGIFY() can be used to stringify literal macro arguments (e.g.
+/// STRINGIFY(__FILE__) becomes "__FILE__"), while XSTRINGIFY() will expand a
+/// macro first (e.g. XSTRINGIFY(__FILE__) becomes "macros.h").
+#define STRINGIFY(s)  #s
+#define XSTRINGIFY(s) STRINGIFY(s)
+
+/// Define a short-hand macros for the location in the code (<file>:<line>)
+#define LOC_ __FILE__ ":" XSTRINGIFY(__LINE__)
+
+#define FUN_ static_cast<const char*>(__PRETTY_FUNCTION__)
+
+#define AT_ std::string(FUN_) + " (" + LOC_ + ")"
+
+#ifdef USE_ASSERTS
+#define ASSERT(condition, message)                                                                                                         \
+  do {                                                                                                                                     \
+    if(!(condition)) {                                                                                                                     \
+      std::cerr << "Assertion `" #condition "` failed in " << __FILE__ << " line " << __LINE__ << ": " << message << std::endl;            \
+      TERMM(1, "ASSERTION FAILED");                                                                                                        \
+    }                                                                                                                                      \
+  } while(false)
+#else
+#define ASSERT(condition, message)                                                                                                         \
+  do {                                                                                                                                     \
+  } while(false && (condition))
+#endif
+
+
+
+#ifdef CLANG_COMPILER
+#pragma clang diagnostic pop
+#endif
+#endif // GRIDGENERATOR_MACROS_H
