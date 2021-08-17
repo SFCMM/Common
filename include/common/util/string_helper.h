1668bac9bdda58743353cea38f3a83e7184ee850
svchb <berger.sven@gmail.com>
2021-08-18 01:51:11 +0200

    fix

--- include/common/util/string_helper.h
+++ include/common/util/string_helper.h
@@ -2,12 +2,12 @@
 
 #ifndef SFCMM_STRING_HELPER_H
 #define SFCMM_STRING_HELPER_H
-#include "common/sfcmm_types.h"
 #include <bitset>
 #include <gcem.hpp>
 #include <iostream>
 #include <map>
 #include <sstream>
+#include "common/sfcmm_types.h"
 
 /// Get a stringstream from a given std::vector. Type needs to overload "<<".
 /// \tparam LENGTH Length of the vector to be streamed.
@@ -15,114 +15,101 @@
 /// \param in Vector to be stringstreamed
 /// \return std::stringstream of the in vector.
 template <GInt LENGTH, class T>
-    static inline auto strStreamify(const std::vector<T> &in) -> std::stringstream {
-      std::stringstream str;
-      str << in[0];
-      for (GInt i = 1; i < LENGTH; i++) {
-        str << " " << in[i];
-      }
-      return str;
-    }
+static inline auto strStreamify(const std::vector<T>& in) -> std::stringstream {
+  std::stringstream str;
+  str << in[0];
+  for(GInt i = 1; i < LENGTH; i++) {
+    str << " " << in[i];
+  }
+  return str;
+}
 
-    /// Get a stringstream from a given std::array. Type needs to overload "<<".
-    /// \tparam LENGTH Length of the vector to be streamed.
-    /// \tparam T Type of the array
-    /// \param in array to be stringstreamed
-    /// \return std::stringstream of the in array.
-    template <GInt LENGTH, class T>
-        static inline auto strStreamify(const std::array<T, LENGTH> &in)
-        -> std::stringstream {
-          std::stringstream str;
-          str << in[0];
-          for (GInt i = 1; i < LENGTH; i++) {
-            str << " " << in[i];
-          }
-          return str;
-        }
+/// Get a stringstream from a given std::array. Type needs to overload "<<".
+/// \tparam LENGTH Length of the vector to be streamed.
+/// \tparam T Type of the array
+/// \param in array to be stringstreamed
+/// \return std::stringstream of the in array.
+template <GInt LENGTH, class T>
+static inline auto strStreamify(const std::array<T, LENGTH>& in) -> std::stringstream {
+  std::stringstream str;
+  str << in[0];
+  for(GInt i = 1; i < LENGTH; i++) {
+    str << " " << in[i];
+  }
+  return str;
+}
 
-        /// Get a stringstream from a given std::array. Type needs to overload "<<".
-        /// \tparam LENGTH Length of the vector to be streamed.
-        /// \tparam T Type of the array
-        /// \param in array to be stringstreamed
-        /// \return std::stringstream of the in array.
-        template <GInt LENGTH>
-        static inline auto strStreamify(const VectorD<LENGTH> &in)
-        -> std::stringstream {
-          std::stringstream str;
-          str << in[0];
-          for (GInt i = 1; i < LENGTH; i++) {
-            str << " " << in[i];
-          }
-          return str;
-        }
+/// Get a stringstream from a given std::array. Type needs to overload "<<".
+/// \tparam LENGTH Length of the vector to be streamed.
+/// \tparam T Type of the array
+/// \param in array to be stringstreamed
+/// \return std::stringstream of the in array.
+template <GInt LENGTH>
+static inline auto strStreamify(const VectorD<LENGTH>& in) -> std::stringstream {
+  std::stringstream str;
+  str << in[0];
+  for(GInt i = 1; i < LENGTH; i++) {
+    str << " " << in[i];
+  }
+  return str;
+}
 
-        /// Convert an input vector to a string vector of the same size or as per the
-        /// given optional argument size. \tparam T Type of the vector. \param in Input
-        /// vector to be stringified. \param size (Default=same as input vector) Provide
-        /// the size if you want partial stringification. \return Vector of strings of
-        /// the input vector.
-        template <typename T>
-        static inline auto toStringVector(const std::vector<T> &in, GInt size = -1)
-        -> std::vector<GString> {
-          std::vector<GString> string_vector;
+/// Convert an input vector to a string vector of the same size or as per the
+/// given optional argument size. \tparam T Type of the vector. \param in Input
+/// vector to be stringified. \param size (Default=same as input vector) Provide
+/// the size if you want partial stringification. \return Vector of strings of
+/// the input vector.
+template <typename T>
+static inline auto toStringVector(const std::vector<T>& in, GInt size = -1) -> std::vector<GString> {
+  std::vector<GString> string_vector;
 
-          if (size == -1) {
-            size = in.size();
-          }
+  if(size == -1) {
+    size = in.size();
+  }
 
-          std::transform(in.begin(), in.begin() + size,
-                         std::back_inserter(string_vector),
-                         [](T b) -> GString { return std::to_string(b); });
-          return string_vector;
-        }
+  std::transform(in.begin(), in.begin() + size, std::back_inserter(string_vector), [](T b) -> GString { return std::to_string(b); });
+  return string_vector;
+}
 
-        /// Convert an input byte vector to a string vector of the same size or as per
-        /// the given optional argument size. <std::byte version> \param in Input byte
-        /// vector to be stringified. \param size (Default=same as input vector) Provide
-        /// the size if you want partial stringification. \return Vector of strings of
-        /// the input byte vector.
-        template <>
-        inline auto toStringVector<std::byte>(const std::vector<std::byte> &in,
-                                              GInt size) -> std::vector<GString> {
-          std::vector<GString> string_vector;
+/// Convert an input byte vector to a string vector of the same size or as per
+/// the given optional argument size. <std::byte version> \param in Input byte
+/// vector to be stringified. \param size (Default=same as input vector) Provide
+/// the size if you want partial stringification. \return Vector of strings of
+/// the input byte vector.
+template <>
+inline auto toStringVector<std::byte>(const std::vector<std::byte>& in, GInt size) -> std::vector<GString> {
+  std::vector<GString> string_vector;
 
-          if (size == -1) {
-            size = static_cast<GInt>(in.size());
-          }
+  if(size == -1) {
+    size = static_cast<GInt>(in.size());
+  }
 
-          std::transform(in.begin(), in.begin() + size,
-                         std::back_inserter(string_vector), [](std::byte b) -> GString {
-            return std::to_string(std::to_integer<GInt>(b));
-          });
-          return string_vector;
-        }
+  std::transform(in.begin(), in.begin() + size, std::back_inserter(string_vector),
+                 [](std::byte b) -> GString { return std::to_string(std::to_integer<GInt>(b)); });
+  return string_vector;
+}
 
-        /// Trim a string from the left.
-        /// \param s Input string
-        /// \return left trimmed string
-        inline auto ltrim(std::string_view s) -> std::string_view {
-          s.remove_prefix(
-              std::distance(s.cbegin(), std::find_if(s.cbegin(), s.cend(), [](int c) {
-                return std::isspace(c) == 0;
-              })));
+/// Trim a string from the left.
+/// \param s Input string
+/// \return left trimmed string
+static inline auto ltrim(std::string_view s) -> std::string_view {
+  s.remove_prefix(std::distance(s.cbegin(), std::find_if(s.cbegin(), s.cend(), [](int c) { return std::isspace(c) == 0; })));
 
-          return s;
-        }
+  return s;
+}
 
-        /// Trim a string from the right.
-        /// \param s Input string
-        /// \return right trimmed string
-        inline auto rtrim(std::string_view s) -> std::string_view {
-          s.remove_suffix(std::distance(
-              s.crbegin(), std::find_if(s.crbegin(), s.crend(),
-                                        [](int c) { return std::isspace(c) == 0; })));
+/// Trim a string from the right.
+/// \param s Input string
+/// \return right trimmed string
+static inline auto rtrim(std::string_view s) -> std::string_view {
+  s.remove_suffix(std::distance(s.crbegin(), std::find_if(s.crbegin(), s.crend(), [](int c) { return std::isspace(c) == 0; })));
 
-          return s;
-        }
+  return s;
+}
 
-        /// Trim a string from both left and the right.
-        /// \param s Input string
-        /// \return Trimmed string
-        inline auto trim(std::string_view s) -> std::string_view { return ltrim(rtrim(s)); }
+/// Trim a string from both left and the right.
+/// \param s Input string
+/// \return Trimmed string
+static inline auto trim(std::string_view s) -> std::string_view { return ltrim(rtrim(s)); }
 
 #endif // SFCMM_STRING_HELPER_H
