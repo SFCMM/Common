27726fca6b1b31aaa44c7d81c93aac05b8f53099
svchb <berger.sven@gmail.com>
2021-12-17 17:42:51 +0100

    fxi unit test

--- UnitTest/test_string_helper.cpp
+++ UnitTest/test_string_helper.cpp
@@ -25,6 +25,6 @@ TEST(toStringVector, HandlesZeroInput) {
   vector<GDouble> testDouble{0.0, 0.1234, 0.35632};
   vector<byte>    testByte{static_cast<byte>(0), static_cast<byte>(1), static_cast<byte>(42)};
 
-  ASSERT_THAT(toStringVector(testDouble), ElementsAre("0.000000", "0.123400", "0.356320"));
+  ASSERT_THAT(toStringVector(testDouble), ElementsAre("0.000000000000000", "0.123400000000000", "0.356320000000000"));
   ASSERT_THAT(toStringVector(testByte), ElementsAre("0", "1", "42"));
 }
